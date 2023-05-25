43ae5e46f4a9369c9bf07ae695bbb46d53386656
Ty-Newman <ltgray579@gmail.com>
2023-05-24 18:20:54 -0700

    Started on add ingredient page

---
+++ react-flask-app/src/components/AddIngredient/AddIngredient.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const AddIngredient = () =>{
+    const [ingredientData, setIngredientData] = useState({
+        ingredientName: '',
+        ingredientCost: '',
+        ingredientImageURL: ''
+    });
+
+
+
+};
+
+export default AddIngredient;
