f7f253b8ad19545035db5e8bb52c3f67fd27a771
Ty-Newman <ltgray579@gmail.com>
2023-05-25 17:23:12 -0700

    finished simple form component for adding ingredients

--- react-flask-app/src/components/AddIngredient/AddIngredient.js
+++ react-flask-app/src/components/AddIngredient/AddIngredient.js
@@ -7,6 +7,38 @@ const AddIngredient = () =>{
         ingredientImageURL: ''
     });
 
+    const handleChange = (e) => {
+        setIngredientData({...ingredientData, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Send data to the db here
+
+        console.log(ingredientData);
+    };
+
+    return(
+        <div className='form-container'>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Field 1:
+                    <input type="text" name="ingredient" value={inputValues.ingredient} onChange={handleChange} />
+                </label>
+                <label>
+                    Field 2:
+                    <input type="text" name="cost" value={inputValues.cost} onChange={handleChange} />
+                </label>
+                <label>
+                    Field 3:
+                    <input type="text" name="ingredientImage" value={inputValues.ingredientImage} onChange={handleChange} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+
+    );
+
 
 
 };
